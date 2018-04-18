fc13c8a7bc62001b5884a354994febe641a225ae
daver35 <daviddelapaz5@gmail.com>
2018-04-18 15:42:09 +0200

    Added Exit prefab

---
+++ MaetGame/Assets/ExitBlockController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitBlockController : MonoBehaviour {
+
+	private Direction exitDirection;
+
+	// Use this for initialization
+	void Start () {
+		if (this.transform.rotation.eulerAngles.y == 0) {
+			exitDirection = Direction.Down;
+		} else if (this.transform.rotation.eulerAngles.y == 90) {
+			exitDirection = Direction.Left;
+		} else if (this.transform.rotation.eulerAngles.y == 180) {
+			exitDirection = Direction.Up;
+		} else if (this.transform.rotation.eulerAngles.y == 270) {
+			exitDirection = Direction.Right;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D collision) {
+		Debug.Log ("hit player");
+		if (collision.gameObject.CompareTag ("Player")) {
+			PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+			Direction dir = player.GetDirection();
+			if(dir == exitDirection){
+				//Finish level
+				Debug.Log ("Exit level");
+				player.StopMovement();
+				player.SetReady(1f);
+			}else{
+				player.StopMovement();
+				player.SetReady(1f);
+			}
+
+		}
+	}
+}
