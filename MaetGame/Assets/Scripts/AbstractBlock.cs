11e172cd6de78c71ce525287e9f698ed96f15cb3
gmolinga <guillem_3_8@hotmail.com>
2018-04-18 09:48:01 +0200

    Add AbstractBlock

---
+++ MaetGame/Assets/Scripts/AbstractBlock.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbstractBlock : MonoBehaviour {
+
+	private AbstractLightScript lightSys;
+
+	// Use this for initialization
+	void Start () {
+		lightSys = GetComponent<AbstractLightScript> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter2d(Collision2D collision) {
+		if (collision.gameObject.CompareTag ("Player")) {
+			lightSys.Light ();
+		}
+	}
+
+	void OnTriggerExit2d(Collision2D collision) {
+		if (collision.gameObject.CompareTag ("Player")) {
+			lightSys.UnLight ();
+		}
+	}
+}
---
+++ MaetGame/Assets/Scripts/AbstractLightScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbstractLightScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//Turn on the lights
+	public void Light(){}
+
+	// Turn off the lights
+	public void UnLight(){}
+
+}
