a6c3df4676c0b8f9bbe3ee5739227abcd458cbf6
gmolinga <guillem_3_8@hotmail.com>
2018-04-18 12:31:41 +0200

    Implemented Player directions

--- MaetGame/Assets/Scripts/LinearBlock.cs
+++ MaetGame/Assets/Scripts/LinearBlock.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LinearBlockController : MonoBehaviour {
-
+	/*
 	private AbstractLightScript lightSys;
 	public int direction;
 
@@ -22,7 +22,7 @@ public class LinearBlockController : MonoBehaviour {
 			lightSys.Light ();
 			PlayerController player = collision.gameObject.GetComponent<PlayerController>();
 			int dir = player.GetDirection();
-			if(direction == 0 && (dir == 1 || dir == -1){
+			if(direction == 0 && (dir == 1 || dir == -1)){
 			    player.stopMovement();
 			    player.ready(0.3f);
 			}else if(direction == 1 && (dir == 2 || dir == -2)){
@@ -37,5 +37,5 @@ public class LinearBlockController : MonoBehaviour {
 		if (collision.gameObject.CompareTag ("Player")) {
 			lightSys.UnLight ();
 		}
-	}
+	}*/
 }
--- MaetGame/Assets/Scripts/PlayerController.cs
+++ MaetGame/Assets/Scripts/PlayerController.cs
@@ -5,17 +5,29 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float velocity;
-	private int direction;
+	private Direction direction;
 	private bool isReady;
 	private Rigidbody2D rigid;
 	private float delay;
+	private bool BlockedUp, BlockedDown, BlockedLeft, BlockedRight;
+	private Animator animator;
+	public enum Direction
+	{
+		up,
+		down,
+		left,
+		right,
+		center
+	};
 
 	//direction getter
-	public int GetDirection() {
+	public Direction GetDirection() {
 		return direction;
 	}
 	// Use this for initialization
 	void Start () {
+		BlockedUp = false; BlockedDown = false; BlockedLeft = false; BlockedRight = false;
+		animator = GetComponent<Animator> ();
 		isReady = true;
 		rigid = GetComponent<Rigidbody2D> ();
 		direction = 0;
@@ -28,15 +40,18 @@ public class PlayerController : MonoBehaviour {
 			
 			float vert = Input.GetAxis ("Vertical");
 			float hori = Input.GetAxis ("Horizontal");
-
-			if (vert != 0) {
-				direction  = (int)vert*2;
-				rigid.velocity = new Vector2 (0, vert * velocity);
-				isReady = false;
-			} else if (hori != 0) {
-				direction = (int)hori;
-				rigid.velocity = new Vector2 (hori * velocity, 0);
-				isReady = false;
+			Debug.Log("Ready :)");
+			if (vert != 0 || hori != 0) {
+				Vector2 vectorDirection = new Vector2(0,0);
+				if (vert != 0) {
+					vectorDirection = new Vector2 (0, vert);
+					isReady = false;
+				} else if (hori != 0) {
+					vectorDirection = new Vector2 (hori, 0);
+					isReady = false;
+				}
+				direction = directionFromVector (vectorDirection);
+				Move (direction);
 			}
 		} else if(delay > 0 ){
 			delay -= Time.deltaTime;
@@ -50,8 +65,76 @@ public class PlayerController : MonoBehaviour {
 		this.delay = delay;
 	}
 
+	public void MoveUp(){
+		if (!BlockedUp) {
+			rigid.velocity = new Vector2 (0, velocity);
+			animator.SetTrigger ("playerVertical");
+			transform.localScale = new Vector3 (1f, 1f, 1f);
+		}
+	}
+	public void MoveDown(){
+		if (!BlockedDown) {
+			rigid.velocity = new Vector2 (0, -velocity);
+			animator.SetTrigger ("playerVertical");
+			transform.localScale = new Vector3 (1f, -1f, 1f);
+		}
+	}
+	public void MoveRight(){
+		if (!BlockedRight) {
+			rigid.velocity = new Vector2 (velocity, 0);
+			animator.SetTrigger ("playerHoritzontal");
+			transform.localScale = new Vector3 (1f, 1f, 1f);
+		}
+	}
+	public void MoveLeft(){
+		if (!BlockedLeft) {
+			rigid.velocity = new Vector2 (-velocity, 0);
+			animator.SetTrigger ("playerHoritzontal");
+			transform.localScale = new Vector3 (-1f, 1f, 1f);
+		}
+	}
+
 	public void StopMovement(){
 		rigid.velocity = new Vector2 (0, 0);
-		direction = 0;
+		direction = Direction.center;
+		animator.SetTrigger ("playerHit");
+	}
+
+	public Direction directionFromVector(Vector2 vector){
+		if (vector.x > 0) {
+			return Direction.right;
+		} else if (vector.x < 0) {
+			return Direction.left;
+		} else if (vector.y > 0) {
+			return Direction.up;
+		} else if (vector.y < 0) {
+			return Direction.down;
+		} else {
+			return Direction.center;
+		}
+	}
+
+	public void BlockDirection(Direction direction){
+		if (direction == Direction.up){
+			
+		}else if(direction == Direction.down){
+			
+		}else if(direction == Direction.right){
+
+		}else if(direction == Direction.left){
+
+		}
+	}
+
+	public void Move(Direction direction){
+		if (direction == Direction.up){
+			MoveUp ();
+		}else if(direction == Direction.down){
+			MoveDown();
+		}else if(direction == Direction.right){
+			MoveRight ();
+		}else if(direction == Direction.left){
+			MoveLeft ();
+		}
 	}
 }
